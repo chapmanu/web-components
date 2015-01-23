4736bfb327b3537da760d394f90e63dcb89aa3c8
James Kerr <jkerr838@gmail.com>
2015-01-23 14:14:29 -0800

    New Version :)

--- dist/cu_components.js
+++ dist/cu_components.js
@@ -327,15 +327,37 @@ $(document).ready(function() {
 
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
+
+	   	/***************
+		* CONFIGURATION
+	   	****************/
+
+	    // How fast should the transiion be?
+	    var transition_speed = 400; // milliseconds
+
+	    // How far before we speed up the scroll FX?
+	    var max_scroll_pixels = 2800; // pixels
+
+       	/***************
+    	* END CONFIGURATION
+       	****************/
 	 
-	    var target = this.hash,
-	    $target = $(target);
+	    var 
+	    target = this.hash,
+		target_offset = $(target).offset().top,
+		window_offset = $(document).scrollTop(),
+		delta = target_offset - window_offset;
 	 
+	    if (Math.abs(delta) > max_scroll_pixels) {
+	    	transition_speed = 100;
+	    }
+
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
+	        'scrollTop': target_offset
+	    }, transition_speed, 'swing', function () {
 	        window.location.hash = target;
 	    });
+	
 	});
 
 });
